4dcfdacf06cf3bb5daf81c6d5441d2b1fb03afc6
Vishwajeet Raj <ext-vishwajeet.raj@graphy.com>
2023-10-04 15:54:49 +0530

    remove logs

--- src/app/api/exp/route.ts
+++ src/app/api/exp/route.ts
@@ -4,6 +4,5 @@ export const GET = async (req: Request, res: Response) => {
     const query = req.url;
     const url = new URL(req.url);
     const chapterId = url.searchParams.get("chapterId");
-    console.log(chapterId)
     return NextResponse.json({ success: true }, { status: 200 })
 }
